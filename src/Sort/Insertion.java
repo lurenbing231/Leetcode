bbb2294da4b773f19ce0ab445055bbc2b60e2190
lurenbing231 <lzhaobetter@163.com>
2017-11-02 11:31:30 +0800

    新增几个Sort

--- src/Sort/Insertion.java
+++ src/Sort/Insertion.java
@@ -5,16 +5,16 @@ package Sort;
  * Created by zhaoliang on 2017/10/26.
  */
 public class Insertion {
-    public static int[] sort(int[] s) {
+    public static int[] sort(int[] s){
         int temp;
-        for (int i = 1; i < s.length; i++) {
+        for (int i = 1;i<s.length;i++){
             temp = s[i];
-            for (int j = i - 1; j >= 0; j--) {
-                if (temp < s[j]) {
-                    s[j + 1] = s[j];
+            for (int j = i-1;j>=0;j--){
+                if (temp<s[j]){
+                    s[j+1] = s[j];
                     s[j] = temp;
-                } else {
-                    s[j + 1] = temp;
+                }else{
+                    s[j+1] = temp;
                     break;
                 }
             }
--- src/Sort/Test.java
+++ src/Sort/Test.java
@@ -6,7 +6,7 @@ package Sort;
 public class Test {
     @org.junit.Test
     public void test() {
-        int[] x = new int[]{5, 9, 7, 8, 1, 5, 6, 7, 10};
+        int[] x = new int[]{5, 9, 7, 8, 1, 5, 6, 7, 10, 4, 8, 15, 64, 54, 85, 99, 74, 32, 52, 64, 36, 56, 84, 16, 75, 98};
         Selection selection = new Selection();
         int[] ints = selection.sort(x);
         for (int i = 0; i < ints.length; i++) {
@@ -23,6 +23,12 @@ public class Test {
         int[] ints2 = shell.sort(x);
         for (int i = 0; i < ints2.length; i++) {
             System.out.print(ints2[i] + " ");
+        }
+        System.out.println();
+        Merge merge = new Merge();
+        int[] ints3 = merge.sort(x);
+        for (int i = 0; i < ints3.length; i++) {
+            System.out.print(ints3[i] + " ");
         }
     }
 }

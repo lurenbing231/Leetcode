b3b1d59e905fda61c6d428922a770e76e72c6bd8
ZhaoLiang <lzhaobetter@163.com>
2018-03-22 21:48:01 +0800

    美图2018-03-22笔试题

---
+++ src/offer/meituan.java
@@ -0,0 +1,45 @@
+package offer;
+
+import java.util.Scanner;
+
+public class meituan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int[] n = new int[str.length()];
+        int[] tmp=new int[10];
+        for (int i = 0; i < str.length(); i++) {
+            Character ch = str.charAt(i);
+            n[i] = Integer.parseInt(ch.toString());
+            tmp[n[i]]++;
+        }
+        for (int i = 1; i < 10; i++) {
+            if (tmp[i]==0){
+                System.out.println(""+i);
+                return;
+            }
+        }
+        if (tmp[0]==0){
+            System.out.println("10");
+            return;
+        }
+        int m=Integer.MAX_VALUE;
+        int s=0;
+        for (int i=1;i<10;i++){
+            if (tmp[i]<m) {
+                m=tmp[i];
+                s=i;
+            }
+        }
+        if (m<tmp[0]){
+            for (int i=0;i<=m;i++){
+                System.out.print(""+s);
+            }
+        }else {
+            System.out.print(""+s);
+            for (int i=0;i<=tmp[0];i++){
+                System.out.print(""+0);
+            }
+        }
+    }
+}
---
+++ src/offer/meituan2.java
@@ -0,0 +1,20 @@
+package offer;
+
+import java.util.Scanner;
+
+public class meituan2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String strs = sc.nextLine();
+        String strt = sc.nextLine();
+        char[] s=strs.toCharArray();
+        char[] t=strt.toCharArray();
+        int num=0;
+        for (int i=0;i<(s.length-t.length+1);i++){
+            for (int j=0;j<t.length;j++){
+                if (s[i+j]!=t[j]) num++;
+            }
+        }
+        System.out.println(""+num);
+    }
+}
